a7d53ec8c25c6a6c9f49b2afa32faf499b1fb302
Levi Nova <levi0fan@mail.ru>
2018-06-11 12:12:47 +0300

    num plus

--- data/themes/index.js
+++ data/themes/index.js
@@ -1,9 +1,11 @@
 const syst = require('./syst');
 const logalg = require('./logalg');
 const arch = require('./arch');
+const numinf = requre('./numinf');
 
 module.exports = {
     syst,
     logalg,
     arch,
+    numinf,
 };
