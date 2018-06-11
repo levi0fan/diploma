e60b2b521674f3963771c109c748025c2e88a731
Levi Nova <levi0fan@mail.ru>
2018-06-11 14:26:09 +0300

    all theory

--- data/themes/index.js
+++ data/themes/index.js
@@ -1,11 +1,21 @@
 const syst = require('./syst');
 const logalg = require('./logalg');
 const arch = require('./arch');
-const numinf = requre('./numinf');
+const numinf = require('./numinf');
+const search = require('./search');
+const model = require('./model');
+const cod = require('./cod');
+const teoralg = require('./teoralg');
+const prog = require('./prog');
 
 module.exports = {
     syst,
     logalg,
     arch,
     numinf,
+    search,
+    model,
+    cod,
+    teoralg,
+    prog,
 };
